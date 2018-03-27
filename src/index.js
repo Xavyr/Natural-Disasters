1b606709aace58564b3ac7a4c49d391eb493c34b
Xavyr Moss <xmoss10@gmail.com>
2018-03-27 10:49:44 -0700

    the initial application setup, run start dev for hot reload

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/AllComponents.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
