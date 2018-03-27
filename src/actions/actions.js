a452b1bb8b2956308b83f4515cafb8982a815f92
Mejin Leechor <amleechor@gmail.com>
2018-03-27 11:57:45 -0700

    Convert to React-Redux

---
+++ src/actions/actions.js
@@ -0,0 +1,9 @@
+import * as types from '../constants/actionTypes';
+
+/* Define action creators (functions that generate action objects) as named exports below
+    EXAMPLE: 
+    export const setDecade = (event) => {
+      type: types.SET_DECADE,
+      payload: event.target.decade
+    };
+*/
---
+++ src/constants/actionTypes.js
@@ -0,0 +1,13 @@
+/**
+ * ************************************
+ *
+ * @module  actionTypes.js
+ * @author
+ * @date
+ * @description Action type constants
+ *
+ * ************************************
+ */
+
+ // Add action type constants below
+ 
