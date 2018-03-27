f4325ad478056eba5f812c081cac671b2228e764
Mejin Leechor <amleechor@gmail.com>
2018-03-27 12:07:39 -0700

    Minor boilerplate fix

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,13 +1,2 @@
-/**
- * ************************************
- *
- * @module  actionTypes.js
- * @author
- * @date
- * @description Action type constants
- *
- * ************************************
- */
-
  // Add action type constants below
  
