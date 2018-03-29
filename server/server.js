4b5800099402fd32d4c7680e09ac0f297f5e877b
Xavyr Moss <xmoss10@gmail.com>
2018-03-28 21:05:13 -0700

    pushing chart modifications

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 //npm modules
 const express = require('express');
-
+var cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
+
 
 
 const path = require('path');
@@ -19,12 +20,14 @@ const disastersController = require('./controllers/disastersController');
 
 // Middleware 
 //pathing to specific files
+app.use(cors());
 app.use(express.static(path.join(__dirname, './../public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // ROUTES
 app.get('/getDisasters', disastersController.getDisasters, (req, res) => {
+	console.log('got here');
 	res.json(res.locals.disasters);
 });
 
--- src/containers/ChartsContainer.jsx
+++ src/containers/ChartsContainer.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import store from '../store';
+//import { LineChart, Line } from 'recharts';
+import axios from 'axios';
 import ChartSettings from '../components/ChartSettings.jsx';
+import ReactChartkick, { LineChart, PieChart, ScatterChart } from 'react-chartkick'
+import Chart from 'chart.js'
+
+ReactChartkick.addAdapter(Chart);
 
 const mapStateToProps = store => ({
   // Pull desired properties from state object into props
@@ -11,15 +17,43 @@ const mapDispatchToProps = dispatch => ({
   // Dispatch actions 
 });
 
+
 class ChartsContainer extends Component {
   constructor(props) {
-    super(props);
+	  super(props);
+	  this.state = {
+		  data: "hello"
+	  };
+
+	  this.inputDataToState = this.inputDataToState.bind(this);
   }
-  
+
+	inputDataToState(data) {
+	  let stateCopy = this.state;
+	  stateCopy.data = data;
+	  this.setState(stateCopy);
+	}
+
+	componentDidMount() {
+  	let that = this;
+		axios.get('http://localhost:3000/getDisasters')
+			.then(function (response) {
+				that.inputDataToState(response.data);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	}
+
   render() {
+  	console.log(this.state);
     return(
       <div>
         <ChartSettings />
+	      <LineChart data={{"2017-01-01": 11, "2017-01-02": 6}} />
+	      <PieChart data={[["Blueberry", 44], ["Strawberry", 23]]} />
+	      <ScatterChart data={[[174.0, 80.0], [176.5, 82.3]]} xtitle="Size" ytitle="Population" />
+
       </div>
     )
   }
