581eb33cae9415e8e5089f59dea860fbaf3dea80
Xavyr Moss <xmoss10@gmail.com>
2018-03-29 15:00:51 -0700

    adding comments

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -2,3 +2,4 @@
 // e.g. export const ACTION_DESCRIPTION = "ACTION_DESCRIPTION";
  export const CHANGE_COUNTRY = 'CHANGE_COUNTRY';
  export const CHANGE_COUNTRY_DATA = 'CHANGE_COUNTRY_DATA';
+ //all actions
